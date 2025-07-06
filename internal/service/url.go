be5a34a7f3b812267aded63cfbb3c1390ad60073
Zhanbolat Yerkinbay <eerzho@gmail.com>
2025-07-06 20:35:00 +0200

    fix: remove logger from service

--- internal/service/url.go
+++ internal/service/url.go
@@ -7,8 +7,6 @@ import (
 	"fmt"
 	"url_shortener/internal/model"
 	"url_shortener/internal/repository"
-
-	"github.com/rs/zerolog/log"
 )
 
 type Url interface {
@@ -28,63 +26,51 @@ func NewUrl(urlRepository repository.Url) Url {
 }
 
 func (u *url) Create(ctx context.Context, longUrl string) (*model.Url, error) {
-	logger := log.With().
-		Str("op", "service.url.Create").
-		Str("long_url", longUrl).
-		Logger()
-	logger.Debug().Msg("creating url")
 	if longUrl == "" {
-		err := errors.New("logn_url is empty")
-		logger.Debug().Err(err).Msg("failed to create url")
+		return nil, errors.New("logn_url is empty")
+	}
+	shortCode, err := u.generateShortCode(ctx, longUrl)
+	if err != nil {
 		return nil, err
 	}
-	shortCode := fmt.Sprintf("%x", sha256.Sum256([]byte(longUrl)))[:6]
-	logger.Debug().Str("short_code", shortCode).Msg("generated short_code")
 	url, err := u.urlRepository.Create(ctx, longUrl, shortCode)
 	if err != nil {
-		logger.Debug().Err(err).Msg("failed to create url")
 		return nil, err
 	}
-	logger.Debug().Int("id", url.Id).Msg("created url")
 	return url, nil
 }
 
 func (u *url) GetByShortCode(ctx context.Context, shortCode string) (*model.Url, error) {
-	logger := log.With().
-		Str("op", "service.url.GetByShortCode").
-		Str("short_code", shortCode).
-		Logger()
-	logger.Debug().Msg("getting url")
 	if shortCode == "" {
-		err := errors.New("short_code is empty")
-		logger.Debug().Err(err).Msg("failed to get url")
-		return nil, err
+		return nil, errors.New("short_code is empty")
 	}
 	url, err := u.urlRepository.GetByShortCode(ctx, shortCode)
 	if err != nil {
-		logger.Debug().Err(err).Msg("failed to get url")
 		return nil, err
 	}
-	logger.Debug().Int("id", url.Id).Msg("got url")
 	return url, nil
 }
 
 func (u *url) GetByShortCodeAndIncrementClicks(ctx context.Context, shortCode string) (*model.Url, error) {
-	logger := log.With().
-		Str("op", "service.url.GetByShortCodeAndIncrementClicks").
-		Str("short_code", shortCode).
-		Logger()
-	logger.Debug().Msg("getting url")
 	if shortCode == "" {
-		err := errors.New("short_code is empty")
-		logger.Debug().Err(err).Msg("failed to get url")
-		return nil, err
+		return nil, errors.New("short_code is empty")
 	}
 	url, err := u.urlRepository.GetByShortCodeAndIncrementClicks(ctx, shortCode)
 	if err != nil {
-		logger.Debug().Err(err).Msg("failed to get url")
 		return nil, err
 	}
-	logger.Debug().Int("id", url.Id).Msg("got url")
 	return url, nil
+}
+
+func (u *url) generateShortCode(ctx context.Context, longUrl string) (string, error) {
+	input := longUrl
+	for attempts := range 5 {
+		shortCode := fmt.Sprintf("%x", sha256.Sum256([]byte(input)))[:6]
+		url, _ := u.urlRepository.GetByShortCode(ctx, shortCode)
+		if url == nil {
+			return shortCode, nil
+		}
+		input = fmt.Sprintf("%s_%d", longUrl, attempts)
+	}
+	return "", errors.New("failed to generate unique short code")
 }
