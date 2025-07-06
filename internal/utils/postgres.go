bb1056d29c10a879ff8f66b3189d206026b6e640
Zhanbolat Yerkinbay <eerzho@gmail.com>
2025-07-07 01:48:02 +0200

    fix: set up postgres connection

--- internal/utils/postgres.go
+++ internal/utils/postgres.go
@@ -1,6 +1,8 @@
 package utils
 
 import (
+	"time"
+
 	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
 	"github.com/rs/zerolog/log"
@@ -11,5 +13,10 @@ func NewPostgresCLient(url string) *sqlx.DB {
 	if err != nil {
 		log.Fatal().Err(err).Msg("failed to connect to postgres")
 	}
+
+	client.SetMaxOpenConns(25)
+	client.SetMaxIdleConns(5)
+	client.SetConnMaxLifetime(5 * time.Minute)
+
 	return client
 }
