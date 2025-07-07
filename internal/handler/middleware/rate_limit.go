4cc474e5294feb0d29dc44b4b6ba1ae7fa80a4fc
Zhanbolat Yerkinbay <eerzho@gmail.com>
2025-07-07 02:09:56 +0200

    fix: fix race condition in rate limit

--- internal/handler/middleware/rate_limit.go
+++ internal/handler/middleware/rate_limit.go
@@ -9,9 +9,24 @@ import (
 	"golang.org/x/time/rate"
 )
 
+type safeLimiter struct {
+	limiter *rate.Limiter
+	mu      sync.Mutex
+}
+
+func newSafeLimiter(limiter *rate.Limiter) *safeLimiter {
+	return &safeLimiter{limiter: limiter}
+}
+
+func (s *safeLimiter) allow() bool {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	return s.limiter.Allow()
+}
+
 type node struct {
 	key   string
-	value *rate.Limiter
+	value *safeLimiter
 	prev  *node
 	next  *node
 }
@@ -39,33 +54,15 @@ func newLruCache(capacity int) *lruCache {
 	}
 }
 
-func (l *lruCache) get(key string) *rate.Limiter {
-	l.mu.Lock()
-	defer l.mu.Unlock()
-
-	item, ok := l.data[key]
-	if !ok {
-		return nil
-	}
-
-	l.remove(item)
-	l.addToHead(item)
-
-	return item.value
-}
-
-func (l *lruCache) put(key string, value *rate.Limiter) {
+func (l *lruCache) getOrPut(key string) *safeLimiter {
 	l.mu.Lock()
 	defer l.mu.Unlock()
 
 	item, ok := l.data[key]
 	if ok {
-		item.value = value
-
 		l.remove(item)
 		l.addToHead(item)
-
-		return
+		return item.value
 	}
 
 	if len(l.data) >= l.capacity {
@@ -73,9 +70,12 @@ func (l *lruCache) put(key string, value *rate.Limiter) {
 		l.remove(l.tail.prev)
 	}
 
+	value := newSafeLimiter(rate.NewLimiter(rate.Limit(10), 20))
 	item = &node{key: key, value: value}
 	l.addToHead(item)
 	l.data[key] = item
+
+	return value
 }
 
 func (l *lruCache) addToHead(item *node) {
@@ -102,13 +102,8 @@ func RateLimit(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		ip := getIp(r)
 
-		limiter := cache.get(ip)
-		if limiter == nil {
-			limiter = rate.NewLimiter(rate.Limit(10), 20)
-			cache.put(ip, limiter)
-		}
-
-		if !limiter.Allow() {
+		limiter := cache.getOrPut(ip)
+		if !limiter.allow() {
 			http.Error(w, http.StatusText(http.StatusTooManyRequests), http.StatusTooManyRequests)
 			return
 		}
