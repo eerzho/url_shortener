8b047e931a1cd614cea0052fb08934bd29b50d71
Zhanbolat <z.yerkinbay@slotegrator.space>
2025-07-09 18:01:49 +0200

    fix: move get ip to middleware.go

--- internal/handler/middleware/middleware.go
+++ internal/handler/middleware/middleware.go
@@ -1,6 +1,10 @@
 package middleware
 
-import "net/http"
+import (
+	"net"
+	"net/http"
+	"strings"
+)
 
 func Chain(h http.Handler, middlewares ...func(http.Handler) http.Handler) http.Handler {
 	for i := len(middlewares) - 1; i >= 0; i-- {
@@ -11,4 +15,23 @@ func Chain(h http.Handler, middlewares ...func(http.Handler) http.Handler) http.
 
 func ChainFunc(h http.HandlerFunc, middlewares ...func(http.Handler) http.Handler) http.Handler {
 	return Chain(h, middlewares...)
+}
+
+func getIp(r *http.Request) string {
+	if forwardedFor := r.Header.Get("X-Forwarded-For"); forwardedFor != "" {
+		parts := strings.Split(forwardedFor, ",")
+		ip := strings.TrimSpace(parts[0])
+		if net.ParseIP(ip) != nil {
+			return ip
+		}
+	}
+	if realIp := r.Header.Get("X-Real-IP"); realIp != "" {
+		if net.ParseIP(realIp) != nil {
+			return realIp
+		}
+	}
+	if ip, _, err := net.SplitHostPort(r.RemoteAddr); err == nil {
+		return ip
+	}
+	return r.RemoteAddr
 }
--- internal/handler/middleware/rate_limit.go
+++ internal/handler/middleware/rate_limit.go
@@ -1,9 +1,7 @@
 package middleware
 
 import (
-	"net"
 	"net/http"
-	"strings"
 	"sync"
 
 	"golang.org/x/time/rate"
@@ -105,26 +103,4 @@ func RateLimit(next http.Handler) http.Handler {
 
 		next.ServeHTTP(w, r)
 	})
-}
-
-func getIp(r *http.Request) string {
-	if forwardedFor := r.Header.Get("X-Forwarded-For"); forwardedFor != "" {
-		parts := strings.Split(forwardedFor, ",")
-		ip := strings.TrimSpace(parts[0])
-		if net.ParseIP(ip) != nil {
-			return ip
-		}
-	}
-
-	if realIp := r.Header.Get("X-Real-IP"); realIp != "" {
-		if net.ParseIP(realIp) != nil {
-			return realIp
-		}
-	}
-
-	if ip, _, err := net.SplitHostPort(r.RemoteAddr); err == nil {
-		return ip
-	}
-
-	return r.RemoteAddr
 }
