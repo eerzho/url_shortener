00c2decd548240c9f5c64129d2e1fca2895cb581
Zhanbolat Yerkinbay <eerzho@gmail.com>
2025-07-11 01:40:14 +0200

    fix: change url response

--- internal/handler/response/url.go
+++ internal/handler/response/url.go
@@ -3,21 +3,33 @@ package response
 import "url_shortener/internal/model"
 
 type Url struct {
-	Id        int    `json:"id"`
 	ShortCode string `json:"short_code"`
 	LongUrl   string `json:"long_url"`
-	Clicks    int    `json:"clicks"`
-	CreatedAt int64  `json:"created_at"`
-	UpdatedAt int64  `json:"updated_at"`
 }
 
 func NewUrl(u *model.Url) *Url {
 	return &Url{
-		Id:        u.Id,
 		ShortCode: u.ShortCode,
 		LongUrl:   u.LongUrl,
-		Clicks:    u.Clicks,
-		CreatedAt: u.CreatedAt.Unix(),
-		UpdatedAt: u.UpdatedAt.Unix(),
+	}
+}
+
+type UrlStats struct {
+	Id          int    `json:"id"`
+	ShortCode   string `json:"short_code"`
+	LongUrl     string `json:"long_url"`
+	CreatedAt   int    `json:"created_at"`
+	UpdatedAt   int    `json:"updated_at"`
+	ClicksCount int    `json:"clicks_count"`
+}
+
+func NewUrlStats(u *model.UrlWithClicksCount) *UrlStats {
+	return &UrlStats{
+		Id:          u.Id,
+		ShortCode:   u.ShortCode,
+		LongUrl:     u.LongUrl,
+		CreatedAt:   int(u.CreatedAt.Unix()),
+		UpdatedAt:   int(u.UpdatedAt.Unix()),
+		ClicksCount: u.ClicksCount,
 	}
 }
