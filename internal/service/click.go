ba802d26d4716a2ce6a3c55ac4df6dcf845863ec
Zhanbolat <z.yerkinbay@slotegrator.space>
2025-07-12 02:57:00 +0200

    fix: use get list metho in click repository

--- internal/service/click.go
+++ internal/service/click.go
@@ -2,22 +2,40 @@ package service
 
 import (
 	"context"
-	"time"
 	"url_shortener/internal/dto"
 	"url_shortener/internal/model"
 )
 
 type Click struct {
+	clickRepository ClickRepository
 }
 
-func NewClick() *Click {
-	return &Click{}
+func NewClick(clickRepository ClickRepository) *Click {
+	return &Click{
+		clickRepository: clickRepository,
+	}
 }
 
 func (c *Click) GetList(ctx context.Context, shortCode string, page, size int) ([]model.Click, *dto.Pagination, error) {
-	clicks := make([]model.Click, 0)
-	for i := range 10 {
-		clicks = append(clicks, model.Click{Id: i, CreatedAt: time.Now()})
+	page = max(page, 1)
+	size = min(max(size, 5), 200)
+	list, total, err := c.clickRepository.GetList(ctx, shortCode, page, size)
+	if err != nil {
+		return nil, nil, err
 	}
-	return clicks, &dto.Pagination{}, nil
+	return list, &dto.Pagination{Page: page, Size: size, Total: total}, nil
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
 }
