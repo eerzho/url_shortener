ef7a93d15a94332229bb60d384357e8215e9fea1
Zhanbolat <z.yerkinbay@slotegrator.space>
2025-07-14 10:36:34 +0200

    fix: small fix logger

--- internal/utils/logger/logger.go
+++ internal/utils/logger/logger.go
@@ -3,20 +3,34 @@ package logger
 import (
 	"log/slog"
 	"os"
+	"slices"
+)
+
+const (
+	EnvProd  = "prod"
+	EnvStage = "stage"
+	EnvDev   = "dev"
 )
 
 func NewLogger(env string) *slog.Logger {
+	if !slices.Contains([]string{EnvProd, EnvStage, EnvDev}, env) {
+		env = EnvProd
+	}
+
 	var level slog.Level
-	if env == "prod" {
+	if env == EnvProd {
 		level = slog.LevelInfo
 	} else {
 		level = slog.LevelDebug
 	}
 
-	opts := &slog.HandlerOptions{Level: level}
+	opts := &slog.HandlerOptions{
+		Level:     level,
+		AddSource: env == EnvDev,
+	}
 
 	var handler slog.Handler
-	if env == "prod" || env == "stage" {
+	if env == EnvProd || env == EnvStage {
 		handler = slog.NewJSONHandler(os.Stdout, opts)
 	} else {
 		handler = slog.NewTextHandler(os.Stdout, opts)
